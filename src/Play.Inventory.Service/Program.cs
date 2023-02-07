96475abae5826da2d065cc9bfc3e8d4e5cc4f656
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-02-07 19:59:31 +0300

    improve exponential backoff technique

--- src/Play.Inventory.Service/Program.cs
+++ src/Play.Inventory.Service/Program.cs
@@ -18,13 +18,15 @@ var services = builder.Services;
 
 builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
 
+Random jitterer = new Random();
+
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:5001");
 })
 .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
     retryCount: 5,
-    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0,1000)),
     onRetry: (outcome, timespan, retryAttempt) => 
     {
         var serviceProvider = services.BuildServiceProvider();
