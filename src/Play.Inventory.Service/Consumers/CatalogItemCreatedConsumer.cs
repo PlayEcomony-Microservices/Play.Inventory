8ee2cac0175be922372ca631f9ffec5214408c9a
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-02-10 21:32:31 +0300

    add update consumer, delete consumer and register necessary services for dependency injection

--- src/Play.Inventory.Service/Consumers/CatalogItemCreatedConsumer.cs
+++ src/Play.Inventory.Service/Consumers/CatalogItemCreatedConsumer.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +10,38 @@ using Play.Inventory.Service.Entities;
 
 namespace Play.Inventory.Service.Consumers
 {
-    public class CatalogItemCreatedConsumer : IConsumer<CatalogItemCreated>
+    public class CatalogItemUpdatedConsumer : IConsumer<CatalogItemUpdated>
     {
         private readonly IRepository<CatalogItem> repository;
 
-        public CatalogItemCreatedConsumer(IRepository<CatalogItem> repository)
+        public CatalogItemUpdatedConsumer(IRepository<CatalogItem> repository)
         {
             this.repository = repository;
         }
-        public async Task Consume(ConsumeContext<CatalogItemCreated> context)
+        public async Task Consume(ConsumeContext<CatalogItemUpdated> context)
         {
             var message = context.Message;
 
             var item = await repository.GetAsync(message.ItemId);
 
-            if(item is not null) return ;
+            if (item is null)
+            {
+                item = new CatalogItem
+                {
+                    Id = message.ItemId,
+                    Name = message.Name,
+                    Description = message.Description
+                };
 
-            item = new CatalogItem{
-                Id= message.ItemId,
-                Name = message.Name,
-                Description = message.Description
-            };
+                await repository.CreateAsync(item);
+            }
+            else
+            {
+                item.Name = message.Name;
+                item.Description = message.Description;
 
-            await repository.CreateAsync(item);
+                await repository.UpdateAsync(item);
+            }
         }
     }
 }
